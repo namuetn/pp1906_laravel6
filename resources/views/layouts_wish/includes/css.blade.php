2c9388257f5cf6980c4347f01d9844bda374649f
Thanh Nam <nam.uetn@gmail.com>
2019-10-11 19:25:45 +0700

    Include

---
+++ resources/views/layouts_wish/includes/css.blade.php
@@ -0,0 +1,15 @@
+<head>
+<title>Wish</title>
+<base href="{{asset('')}}">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Wish shop project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+@yield('css_home')
+@yield('css_category')
+@yield('css_cart')
+@yield('css_checkout')
+@yield('css_contact')
+@yield('css_product')
+</head>
