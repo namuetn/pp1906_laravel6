7a8a9b2019973382b00c11077aa1cf66586e5ac7
Thanh Nam <nam.uetn@gmail.com>
2019-10-18 20:45:03 +0700

    Update, destroy, delete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
+    	'user_id',
     	'name',
     	'content',
     	'quantity',
     	'price'
     ];	
+
+   
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function orders() {
+        return $this->belongsToMany('App\Models\Order', 'product_order');
+    }
 }
